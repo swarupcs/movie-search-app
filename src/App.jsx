f6bfd34df08f9e5c391409271e4a3a93bda7473a
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-08-09 22:57:30 +0530

    Add Navbar component and styles; implement dark/light mode variables in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
 import MainRoutes from './routes/MainRoutes'
 
 function App() {
 
   return (
     <>
+    <Navbar/>
     <MainRoutes/>
     </>
   )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar-wrapper">
+      <div>Movie Base</div>
+      <div className="search-bar">
+        <input type='text' />
+      </div>
+      <div>Theme</div>
+    </div>
+  );
+}
+
+export default Navbar;
